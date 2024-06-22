b9b1e46a3bbfd4f8ab64b0830de19c5aecd72766
Lev1ty <adamxhy123@outlook.com>
2024-06-22 14:19:26 -0400

    read sample from database

--- go/go.mod
+++ go/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/bufbuild/protovalidate-go v0.6.2
 	github.com/clerk/clerk-sdk-go/v2 v2.0.4
 	github.com/dotenv-org/godotenvvault v0.6.0
+	github.com/jackc/pgx/v5 v5.6.0
 	github.com/klauspost/compress v1.17.9
 	github.com/ulule/limiter/v3 v3.11.2
 	golang.org/x/net v0.26.0
@@ -18,11 +19,15 @@ require (
 	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
 	github.com/go-jose/go-jose/v3 v3.0.1 // indirect
 	github.com/google/cel-go v0.20.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/stoewer/go-strcase v1.3.0 // indirect
 	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240401170217-c3f982113cda // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240401170217-c3f982113cda // indirect
--- go/pb/lmsysmd/sample/v1/sample.go
+++ go/pb/lmsysmd/sample/v1/sample.go
@@ -2,30 +2,48 @@ package sample
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"sync"
 
 	"connectrpc.com/connect"
 	samplev1 "github.com/Lev1ty/lmsysmd/pbi/lmsysmd/sample/v1"
 	"github.com/Lev1ty/lmsysmd/pbi/lmsysmd/sample/v1/samplev1connect"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type SampleService struct {
 	samplev1connect.UnimplementedSampleServiceHandler
+	dbOnce sync.Once
+	db     *pgxpool.Pool
 }
 
 func (ss *SampleService) GetSample(
 	ctx context.Context,
 	req *connect.Request[samplev1.GetSampleRequest],
 ) (*connect.Response[samplev1.GetSampleResponse], error) {
-	return connect.NewResponse(&samplev1.GetSampleResponse{Sample: &samplev1.Sample{
-		SampleId: 1,
-		Content:  "Compare the Ground Truth answer to the proposed Differential Diagnosis. Select the highest ranked correct answer.",
-		Choices: []*samplev1.Sample_Choice{
-			{ChoiceId: 1, Content: "Paraganglioma"},
-			{ChoiceId: 2, Content: "Schwannoma"},
-			{ChoiceId: 3, Content: "Carotid body tumor"},
-			{ChoiceId: 4, Content: "Glomus vagale tumor"},
-			{ChoiceId: 5, Content: "Metastatic lymphadenopathy"},
-		},
-		Truth: "Glomus vagale tumor",
-	}}), nil
+	ss.dbOnce.Do(func() {
+		var err error
+		if ss.db, err = pgxpool.New(ctx, os.Getenv("POSTGRES_DSN")); err != nil {
+			panic(err)
+		}
+	})
+	sam := &samplev1.Sample{}
+	if err := ss.db.QueryRow(ctx, "SELECT id, content, truth FROM samples WHERE id = $1", req.Msg.GetSampleId()).Scan(&sam.SampleId, &sam.Content, &sam.Truth); err != nil {
+		return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("get sample %d: %w", req.Msg.GetSampleId(), err))
+	}
+	rs, err := ss.db.Query(ctx, "SELECT id, content FROM sample_choices WHERE sample_id = $1", req.Msg.GetSampleId())
+	if err != nil {
+		return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("list sample choices %d: %w", req.Msg.GetSampleId(), err))
+	}
+	defer rs.Close()
+	for rs.Next() {
+		var id uint32
+		var content string
+		if err := rs.Scan(&id, &content); err != nil {
+			return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("scan sample choice %d: %w", req.Msg.GetSampleId(), err))
+		}
+		sam.Choices = append(sam.Choices, &samplev1.Sample_Choice{ChoiceId: id, Content: content})
+	}
+	return connect.NewResponse(&samplev1.GetSampleResponse{Sample: sam}), nil
 }
